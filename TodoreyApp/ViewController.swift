0e93043cc274d5b05f8f0c7c467164c63dff528b
Sudheer Kumar <sudheer.sap31@gmail.com>
2019-02-02 08:31:00 +0000

    Initial Commit

---
+++ TodoreyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoreyApp
+//
+//  Created by Sudheer  Pendyala on 02/02/2019.
+//  Copyright © 2019 Sudheer  Pendyala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
